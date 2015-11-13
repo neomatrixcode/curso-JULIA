9a5ac56e8728ff063d0329ae3c5cfb621dc7a61e
neomatrix <josuecevedo@gmail.com>
2015-11-13 14:33:11 -0600

    drogasduras!!!

---
+++ drogasduras.jl
@@ -0,0 +1,15 @@
+a=1
+b=2
+c=3
+
+type animal
+x::String
+y::String
+end
+
+a = {"hola"=>2, "adios"=>x->x+1, "animal"=> animal("oso","azul"), "arreglo" => [ eval(:(($op)(($op)(a,b),c))) for op = (:+, :*, :&, :|, :$) ] }
+
+
+ja={"locuras"=>[a,1,"hola"]}
+
+map(ja["locuras"][1]["adios"], ja["locuras"][1]["arreglo"])
